d73f11ca9f99c90f594486bc25d78a16754aea37
Rohan Kaicker <rkaicker@myseneca.ca>
2022-09-10 12:48:03 -0400

    further defined options, and logic to determine if path is file or folder

--- rohan-ssg.js
+++ rohan-ssg.js
@@ -1,6 +1,53 @@
 #!/usr/bin/env node
 
+// Node filesystem module
+const fs = require('fs');
+
 const { Command } = require('commander');
 const program = new Command();
+
+// get version from package.json file
+// - based on https://stackoverflow.com/questions/41123631/how-to-get-the-version-from-the-package-json-in-typescript
+const pj = require('./package.json');
+const version = pj.version;
+
+program
+  .option('-v, --version', 'current program version')
+  .option('-h, --help', 'program instructions')
+  .option('-i, --input <input>', 'input folder or file');
+
+program.parse();
+
+const options = program.opts();
+
+if (options.version){
+  console.log(`The current version is: ${version}`);
+}
+
+if (options.help){
+  console.log(`Instructions: `);
+}
+
+if (options.input){
+  // get path passed to program
+  const path = options.input;
+  // check if path is a file or a folder
+  // - based on https://stackoverflow.com/questions/15630770/node-js-check-if-path-is-file-or-directory
+  fs.lstat(path, (err, stats) => {
+    if (err){
+      return console.log(err);
+    }
+
+    // if the path points to a file
+    if (stats.isFile()){
+      
+    }
+
+    // if the path points to a directory (folder)
+    if (stats.isDirectory()){
+
+    }
+  });
+}
 
 
