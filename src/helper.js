7e75863be343cc5fec6c40d4c8e16df6912e7702
Rohan Kaicker <rkaicker@myseneca.ca>
2022-10-15 16:51:09 -0400

    (upgrades) - separated text file handling, separated markdown file handling, fs.rmSync() to recursively delete directories/ files

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
+const text = require('./utils/textHandler.js');
+const md = require('utils/markdownHandler.js');
 
 function punctuationCheck(str){
   if (str.slice(-1) === '.' || str.slice(-1) === '!' || str.slice(-1) === '?' || str.slice(-1) === ',' || str.slice(-1) === ';' || str.slice(-1) === ':'){
@@ -17,15 +19,13 @@ function generateSite(file, lang, stylesheet = ''){
   if (ext !== '.txt' && ext !== '.md'){
     return
   }
-  // create /dist folder if it doesn't exist
-  if (!fs.existsSync('./dist')){
-    fs.mkdirSync('./dist');
-  } else {
-    // if it does exist, delete all files in it (folder must be empty on each run)
-    fs.readdirSync('./dist').forEach((file) => {
-      fs.unlinkSync(`./dist/${file}`);
-    });
+
+  // create /dist folder if it doesn't exist, if it does exist delete the directory and all subdirectories and files
+  if (fs.existsSync('dist')){
+    fs.rmSync('./dist', { recursive: true });
   }
+  fs.mkdirSync('dist');
+
 
   const output = './dist/' + fileName + '.html';
   const inputStream = fs.createReadStream(file);
@@ -56,79 +56,10 @@ function generateSite(file, lang, stylesheet = ''){
 
   rl.on('line', (line) => {
     if (ext == '.txt') {
-      if (line.length === 0){
-        emptyLines++;
-      }
-      else {
-        if (emptyLines === 2 && !titleFound){
-          title = text;
-          titleFound = true;
-          text = line;
-          emptyLines = 0;
-        }
-        else if (emptyLines > 0 && titleFound){
-          body += `
-          <p>${text}</p>
-          `;
-          text = (line + ' ');
-          emptyLines = 0;
-        } 
-        else {
-          text += (line + ' ');
-        }
+      text.textHandler(emptyLines, title, text, titleFound, body, line);
     }
-  }
     else if (ext == '.md') {
-      //check if the line is a heading 1
-      fileMarkdown = true;
-      if (line.startsWith('# '))
-      {
-        //cut the '# ' out of the line
-        text = line.substring(2);
-        body += `
-        <h1>${text}</h1>
-        `;
-      }
-      else if (line.startsWith('## '))
-      {
-        //cut the '## ' out of the line
-        text = line.substring(3);
-        body += `
-        <h2>${text}</h2>
-        `;
-      }
-      else if (line === '---'){
-        body += `
-        <hr>
-        `;
-      }
-      else if (line.startsWith('> '))
-      {
-        //cut the '> ' out of the line
-        text = line.substring(2);
-        body += `
-        <blockquote>${text}</blockquote>
-        `;
-      }
-      else if (line != "")
-      {
-        //convert markdown link to href 
-        line = line.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
-        //convert markdown bold ** to html <b> element
-        line = line.replace(/\*\*(.*)\*\*/g, '<b>$1</b>');
-        //convert markdown bold __ to html <b> element
-        line = line.replace(/__(.*)__/g, '<b>$1</b>');
-        //convert markdown italics * to html <i> element
-        line = line.replace(/\*(.*)\*/g, '<i>$1</i>');
-        //convert markdown italics _ to html <i> element
-        line = line.replace(/_(.*)_/g, '<i>$1</i>');
-        //convert markdown code ` to html <code> element
-        line = line.replace(/`(.*?)`/g, '<code>$1</code>');
-        
-        body += `
-        <p>${line}</p>
-        `;
-      }
+      md.markdownHandler(fileMarkdown, text, body);
     }
   });
   
---
+++ src/utils/textHandler.js
@@ -0,0 +1,27 @@
+const textHandler = (emptyLines, title, text, titleFound, body, line) => {
+  if (line.length === 0){
+    emptyLines++;
+  }
+  else {
+    if (emptyLines === 2 && !titleFound){
+      title = text;
+      titleFound = true;
+      text = line;
+      emptyLines = 0;
+    }
+    else if (emptyLines > 0 && titleFound){
+      body += `
+      <p>${text}</p>
+      `;
+      text = (line + ' ');
+      emptyLines = 0;
+    } 
+    else {
+      text += (line + ' ');
+    }
+  }
+}
+
+module.exports = {
+  textHandler
+};
